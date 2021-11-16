6a77557ee724737590d209d4348745ebdb3f94d8
rivsidn <rivsidn@163.com>
2021-11-16 23:16:12 +0800

    tracepoint

--- debug/tracepoint/tracepoint.c
+++ debug/tracepoint/tracepoint.c
@@ -1,4 +1,9 @@
 
+
+
+/*
+ * tracepoint.h 中定义
+ */
 #define __DECLARE_TRACE(name, proto, args, data_proto, data_args)	\
 	extern struct tracepoint __tracepoint_##name;			\
 	static inline void trace_##name(proto)				\
@@ -26,17 +31,17 @@
 	}
 
 
-
 #define DECLARE_TRACE(name, proto, args)				\
 		__DECLARE_TRACE(name, PARAMS(proto), PARAMS(args),	\
 				PARAMS(void *__data, proto),		\
 				PARAMS(__data, args))
 
-
 #define TRACE_EVENT(name, proto, args, struct, assign, print)	\
 	DECLARE_TRACE(name, PARAMS(proto), PARAMS(args))
 
-
+/*
+ * include/trace/events/skb.h 中定义
+ */
 TRACE_EVENT(kfree_skb,
 
 	TP_PROTO(struct sk_buff *skb, void *location),
